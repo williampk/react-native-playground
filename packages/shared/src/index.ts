b25fd307ba25e155ee07cf243676b2f7be83350c
William P Khamooshi <william.khamooshi@gmail.com>
2025-08-25 10:44:37 +0100

    initialized the project

---
+++ packages/shared/src/index.ts
@@ -0,0 +1,10 @@
+export const sharedFunction = () => {
+    console.log("This is a shared function.");
+};
+
+export const sharedConstant = "This is a shared constant.";
+
+export interface SharedInterface {
+    id: number;
+    name: string;
+}
